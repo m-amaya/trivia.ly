1fd82b7ef9628cd8909d78796c1b88518ddb3ad4
Marissa Amaya <mamaya.dev@gmail.com>
2024-08-06 18:42:26 -0500

    setup login page

--- src/pages/login/Login.page.tsx
+++ src/pages/login/Login.page.tsx
@@ -1,5 +1,14 @@
-import { AppShell } from "@mantine/core";
+import {
+  AppShell,
+  Button,
+  Flex,
+  Grid,
+  Group,
+  Image,
+  Text
+} from "@mantine/core";
 import { Helmet } from "react-helmet";
+import classes from "./Login.module.css";
 
 export function LoginPage() {
   return (
@@ -7,7 +16,45 @@ export function LoginPage() {
       <Helmet>
         <title>Login | Trivia.ly</title>
       </Helmet>
-      <AppShell.Main>Login Page</AppShell.Main>
+      <AppShell className={classes.shell} p="lg">
+        <Flex flex={1} justify="center">
+          <Group>
+            <Image h={70} src="/logo.png" alt="Logo image" />
+            <Text
+              c="turquoise.1"
+              className={classes.title}
+              ff="heading"
+              fw={700}
+            >
+              Trivia.ly
+            </Text>
+          </Group>
+        </Flex>
+        <Flex bg="ocean.6" className={classes.bottom} flex={1} p="lg">
+          <Grid columns={4} w="100%">
+            <Grid.Col span={{ base: 4, sm: 2, md: 1 }}>
+              <Button fullWidth size="lg">
+                Login with Google
+              </Button>
+            </Grid.Col>
+            <Grid.Col span={{ base: 4, sm: 2, md: 1 }}>
+              <Button fullWidth size="lg">
+                Login with Microsoft
+              </Button>
+            </Grid.Col>
+            <Grid.Col span={{ base: 4, sm: 2, md: 1 }}>
+              <Button fullWidth size="lg">
+                Login with Apple
+              </Button>
+            </Grid.Col>
+            <Grid.Col span={{ base: 4, sm: 2, md: 1 }}>
+              <Button fullWidth size="lg">
+                Login with GitHub
+              </Button>
+            </Grid.Col>
+          </Grid>
+        </Flex>
+      </AppShell>
     </>
   );
 }
--- src/tokens/theme.ts
+++ src/tokens/theme.ts
@@ -1,4 +1,5 @@
 import {
+  Button,
   createTheme,
   CSSVariablesResolver,
   DEFAULT_THEME,
@@ -9,16 +10,76 @@ const themeOverride = createTheme({
   autoContrast: true,
   colors: {
     blue: [
-      "#ebfaff",
-      "#d7f2fb",
-      "#a9e3f9",
-      "#7ad4f7",
-      "#5bc8f6",
-      "#4cc0f5",
-      "#43bdf6",
-      "#37a6dc",
-      "#2893c4",
-      "#0080ad"
+      "#ccfcff",
+      "#9ff6ff",
+      "#5cecff",
+      "#13d9fd",
+      "#00bce3",
+      "#0095bf",
+      "#04769a",
+      "#0e607d",
+      "#104f69",
+      "#043448"
+    ],
+    ocean: [
+      "#cefffd",
+      "#a4fdfb",
+      "#65fbfa",
+      "#1feef1",
+      "#03c6cb",
+      "#06a7b4",
+      "#0c8592",
+      "#146b76",
+      "#155864",
+      "#073a45"
+    ],
+    orange: [
+      "#ffe6d4",
+      "#ffc9a8",
+      "#ffa371",
+      "#ff7138",
+      "#fe4b11",
+      "#ef3107",
+      "#c62008",
+      "#9d1c0f",
+      "#7e1a10",
+      "#440906"
+    ],
+    red: [
+      "#ffdde2",
+      "#ffc0ca",
+      "#ff94a4",
+      "#ff5771",
+      "#ff2344",
+      "#ff193c",
+      "#d70021",
+      "#b1031d",
+      "#920a1f",
+      "#50000c"
+    ],
+    turquoise: [
+      "#c3fff6",
+      "#87ffef",
+      "#43ffe6",
+      "#0bedd0",
+      "#00d7bd",
+      "#00ae9d",
+      "#008a7f",
+      "#026d66",
+      "#085954",
+      "#003736"
+    ],
+    yellow: [
+      "#fffec1",
+      "#fff986",
+      "#ffed41",
+      "#ffdc0d",
+      "#ffcd00",
+      "#d19600",
+      "#a66b02",
+      "#89530a",
+      "#74440f",
+      "#442304"
     ]
   },
   cursorType: "pointer",
@@ -26,15 +87,55 @@ const themeOverride = createTheme({
   focusRing: "auto",
   fontFamily: "'Inter Variable', sans-serif",
   headings: {
-    fontFamily: "'Inter Variable', sans-serif"
+    fontFamily: "'Lexend Variable', sans-serif"
   },
-  primaryColor: "blue"
+  primaryColor: "turquoise",
+  other: {
+    blue: {
+      white: "#ebffff"
+    },
+    ocean: {
+      white: "##ecfffe"
+    },
+    orange: {
+      white: "#fff4ed"
+    },
+    red: {
+      white: "#fff0f2"
+    },
+    turquoise: {
+      white: "#eefffb"
+    },
+    yellow: {
+      white: "#fffee7"
+    }
+  },
+  components: {
+    Button: Button.extend({
+      styles: {
+        label: {
+          color: "var(--mantine-button-color)"
+        },
+        root: {
+          "--button-bg": "var(--mantine-button-bg)",
+          "--button-hover": "var(--mantine-button-hover)",
+          transition: "background-color 200ms"
+        }
+      }
+    })
+  }
 });
 
 export const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
 
-export const resolver: CSSVariablesResolver = () => ({
-  variables: {},
-  light: {},
+export const resolver: CSSVariablesResolver = (theme) => ({
+  variables: {
+    "--mantine-border-radius": `${theme.defaultRadius}px`
+  },
+  light: {
+    "--mantine-button-bg": theme.colors.turquoise[3],
+    "--mantine-button-color": theme.colors.turquoise[9],
+    "--mantine-button-hover": theme.colors.turquoise[1]
+  },
   dark: {}
 });
